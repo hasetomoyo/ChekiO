e892a716027cbe67c2363d1bcd6f747133435353
GMOPG\E024779 <hasegawa@gmo-pg.com>
2017-08-30 16:50:20 +0900

    ２つ目クリア！

--- HOME/Non-unique_Elements.py
+++ HOME/Non-unique_Elements.py
@@ -1,1 +1,23 @@
+def checkio(data):
 
+    check_data = {}
+        
+    for k in data:
+        check_data.setdefault(k, 0)
+        check_data[k] = check_data[k] + 1
+    print(check_data)
+
+    delte_obj = []
+    for k, v in check_data.items():
+        if v == 1:
+            delte_obj.append(k)
+    print(delte_obj)
+
+    result_data = []
+    for k in data:
+        if k not in delte_obj:
+            result_data.append(k)
+
+    return result_data
+
+print(checkio([1, 2, 3, 4, 5]))
